5ca22e4b8cee0c1ce877c4dcae79e844333c5ea2
clamp123 <clampo2002@gmail.com>
2021-06-28 21:10:12 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -38,6 +38,7 @@ debugger
   var theleftpaddle = pongstuff("#theleftpaddle");
   var therightpaddle = pongstuff("#therightpaddle");
   
+  
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
