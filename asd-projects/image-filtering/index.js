c7ef358b31a3ff92fda6ea93c90abd532269630d
clamp123 <clampo2002@gmail.com>
2022-06-22 15:42:46 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -21,7 +21,8 @@ function resetAndRender() {
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
   applyFilter(reddify);
-
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
 
     // do not change the below line of code
     render($("#display"), image);
